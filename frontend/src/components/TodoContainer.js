fd35785ae77d1bb9b271a4eb9a9964ac807bfe8e
Cod-kumar <1606.nk@gmail.com>
2021-07-12 22:55:49 +0530

    v0.0.1

--- frontend/src/components/TodoContainer.js
+++ frontend/src/components/TodoContainer.js
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosAdd } from "react-icons/io";
 import Todolist from './Todolist.js'
 
 const TodoContainer = () =>{
+
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const inputHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos, {text: inputText, completed: false, id: Math.random() *1000}
+        ]);
+        setInputText("");
+    }
+
     return(
         <div>
             <div id="todo-input">
                 <form action="" className="col-lg-12">
-                    <input type="text" placeholder="Write here.." className="todo-inp" required/>
+                    <input type="text" value={ inputText } onChange={ inputHandler } placeholder="Write here.." className="todo-inp" required/>
                     
                     <label htmlFor="todo-inp" className="todo-add">
                         <IoIosAdd className="todo-i"/>
-                        <input type="submit" name="todo-inp" value=" "/> 
+                        <input type="submit" name="todo-inp" onClick={ submitHandler } value=" "/> 
                     </label>
                 </form>
             </div>
 
-            {/* Todo list below */}
-            <Todolist />
-            <Todolist />
-            <Todolist />
-            <Todolist />
-            <Todolist />
+            {todos.map((todolist) =>(
+                <Todolist key={todolist.id} text={todolist.text}/>
+            ))}
+            
         </div>
     )
 }
--- frontend/src/components/Todolist.js
+++ frontend/src/components/Todolist.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { IoIosCloseCircleOutline } from "react-icons/io";
 // import { TiDeleteOutline } from "react-icons/all-files";
 
-const Todolist = () =>{
+const Todolist = ({ text }) =>{
     return(
         <div className="col-lg-12">
             <form action="" className="todo-list-con">
                 <input type="checkbox" className="checkbox" />
-                <span className="todo-title">Learn about React</span>
+                <span className="todo-title">{ text }</span>
                 
                 <button type="button" className="delete-btn">
                     <IoIosCloseCircleOutline fontSize="20" color="red"/>
