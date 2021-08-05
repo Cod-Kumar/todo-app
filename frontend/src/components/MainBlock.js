c1ff9b618321b2d10a5aba1031bc9227847905e7
Cod-kumar <1606.nk@gmail.com>
2021-08-06 00:28:18 +0530

    Count fix

--- frontend/src/components/MainBlock.js
+++ frontend/src/components/MainBlock.js
@@ -5,8 +5,6 @@ import Todolist from './Todolist';
 const MainBlock = () =>{
     return(
        <div id="main_block" className="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-10 offset-1"> 
-
-
             {/* Todo Container */}
             <TodoContainer />
        </div>
